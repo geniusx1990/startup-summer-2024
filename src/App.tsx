07f0f3ea3ff11ecd3044ba301423094ec9a84f5e
Matsvei Khmialnitski <geniusx1990@gmail.com>
2024-05-07 12:11:35 +0300

    init: start new project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+import "./App.css";
+
+export default function App() {
+  return <MantineProvider>{/* Your app here */}</MantineProvider>;
+}
